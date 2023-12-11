b7413c052344c57df17018d07453fb89c452fc92
任帅 <1064425721@qq.com>
2023-12-11 11:12:21 +0800

    chore: clean

--- .cz-config.js
+++ .cz-config.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   types: [
-    { value: 'feat', name: 'feat:     新功能' },
-    { value: 'fix', name: 'fix:      修复' },
-    { value: 'docs', name: 'docs:     文档变更' },
-    { value: 'style', name: 'style:    代码格式(不影响代码运行的变动)' },
+    { value: 'feat', name: 'feat: 新功能' },
+    { value: 'fix', name: 'fix: 修复' },
+    { value: 'docs', name: 'docs: 文档变更' },
+    { value: 'style', name: 'style: 代码格式(不影响代码运行的变动)' },
     { value: 'refactor', name: 'refactor: 重构(既不是增加feature，也不是修复bug)' },
-    { value: 'perf', name: 'perf:     性能优化' },
-    { value: 'test', name: 'test:     增加测试' },
-    { value: 'chore', name: 'chore:    构建过程或辅助工具的变动' },
+    { value: 'perf', name: 'perf: 性能优化' },
+    { value: 'test', name: 'test: 增加测试' },
+    { value: 'chore', name: 'chore: 构建过程或辅助工具的变动' },
   ],
   // override the messages, defaults are as follows
   messages: {
